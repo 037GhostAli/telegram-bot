0aed629bd86a04bc742a6e29a7c6b3834ff66a60
GhostAli <shayan.nas206@outlook.com>
2025-12-18 10:40:25 +0330

    bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+TOKEN = "8477157715:AAHpa85swihvZlGpjU7P8Jnh2-R1MnmJ6ug"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = InlineKeyboardMarkup()
+
+    webapp = WebAppInfo(
+        url="https://037ghostali.github.io/Free"
+    )
+
+    button = InlineKeyboardButton(
+        text="🎁 ادامه و ورود به طرح ویژه",
+        web_app=webapp
+    )
+
+    markup.add(button)
+
+    bot.send_message(
+        message.chat.id,
+        "🎉 به ربات اینترنت رایگان یکساله خوش آمدید!\n\n"
+        "برای ادامه روی دکمه زیر کلیک کنید 👇",
+        reply_markup=markup
+    )
+
+bot.polling()
